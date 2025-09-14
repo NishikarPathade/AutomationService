8ce9a52acc4840a40d7de94e65823f384e0b4fa9
Nishikar Pathade <122215484+NishikarPathade@users.noreply.github.com>
2025-09-15 00:07:01 +0530

    Create Resources.js component with placeholder for resource and download area.

---
+++ src/components/Resources.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Resources = () => {
+    return (
+        <div>
+            <h1>Resources</h1>
+            <div>
+                {/* Placeholder for resource */}
+                <p>Resource details will go here.</p>
+            </div>
+            <div>
+                {/* Placeholder for download area */}
+                <h2>Download Area</h2>
+                <a href="#">Download Link</a>
+            </div>
+        </div>
+    );
+};
+
+export default Resources;

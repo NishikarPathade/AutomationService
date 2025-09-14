90df137614d33998a8cff950e571cbd98b33c508
Nishikar Pathade <122215484+NishikarPathade@users.noreply.github.com>
2025-09-15 00:05:44 +0530

    Create Home.js for the main introduction page

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Automation Service Engineering</h1>
+            <p>This website is a resource for Automation Service Engineers, providing information on various PLCs and programming guides.</p>
+        </div>
+    );
+};
+
+export default Home;
